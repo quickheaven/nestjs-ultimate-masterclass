16b169e4023cea6831b0ab1ab7a238c2569e99e4
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-05 16:05:02 -0400

    19 Services: Where Business Logic Lives

--- nestjs-intro/src/tasks/tasks.controller.ts
+++ nestjs-intro/src/tasks/tasks.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
   @Get()
   public findAll(): string[] {
     return ['A', 'B'];
