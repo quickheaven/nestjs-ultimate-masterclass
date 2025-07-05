52ab207906e9f219727651abbc477cae2305b5c0
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-05 16:00:05 -0400

    18 Working with Dynamic Data in URLs (Route Parameters)

--- nestjs-intro/src/tasks/tasks.controller.ts
+++ nestjs-intro/src/tasks/tasks.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
   @Get()
   public findAll(): string[] {
     return ['A', 'B'];
+  }
+
+  /*
+  @Get('/:id')
+  public findOne(@Param() params: any): string {
+    return `This number is ${params.id}`;
+  }
+  */
+  @Get('/:id')
+  public findOne(@Param('id') id: string): string {
+    return `This number is ${id}`;
   }
 }
