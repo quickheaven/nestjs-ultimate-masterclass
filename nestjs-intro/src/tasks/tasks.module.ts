9977e3a6918c5f57a50900e41740724854b20129
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-07 18:50:31 -0400

    52 Understanding forFeature: Repository Creation & Module Bounderies

--- nestjs-intro/src/tasks/tasks.module.ts
+++ nestjs-intro/src/tasks/tasks.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])], // Add your entities here if needed
   controllers: [TasksController],
   providers: [TasksService],
 })
