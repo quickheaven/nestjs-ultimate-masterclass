0f985eaeb1f693aab5b478c3cb502ec61dec7f9a
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-11 22:08:28 -0400

    61 Validating Nested Object Arrays in DTOs

---
+++ nestjs-intro/src/tasks/create-task-label.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateTaskLabelDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  name: string;
+  taskId: string;
+}
