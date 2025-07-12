a8efa3556b96b6923a8779d5e4012624201d7f07
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-11 21:55:00 -0400

    59 Ensuring Unique Labels Per Task

--- nestjs-intro/src/tasks/task-label.entity.ts
+++ nestjs-intro/src/tasks/task-label.entity.ts
@@ -2,13 +2,16 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  Index,
   ManyToOne,
   PrimaryGeneratedColumn,
+  Unique,
   UpdateDateColumn,
 } from 'typeorm';
 import { Task } from './task.entity';
 
 @Entity()
+@Unique(['name', 'taskId'])
 export class TaskLabel {
   @PrimaryGeneratedColumn('uuid')
   id: string;
@@ -16,8 +19,12 @@ export class TaskLabel {
   @Column()
   name: string;
 
+  @Column()
+  @Index()
+  taskId: string;
+
   @ManyToOne(() => Task, (task) => task.labels)
-  taskId: Task;
+  task: Task;
 
   @CreateDateColumn()
   createdAt: Date;
