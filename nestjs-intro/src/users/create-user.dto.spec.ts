d62ca7d6676149c8c1cd3c09e0670cb23eb405a2
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-24 19:24:30 -0400

    83 Writing First Unit Test for DTO

--- nestjs-intro/src/users/create-user.dto.spec.ts
+++ nestjs-intro/src/users/create-user.dto.spec.ts
@@ -1,5 +1,22 @@
+import { validate } from 'class-validator';
+import { CreateUserDto } from './create-user.dto';
+
 describe('CreateUserDto', () => {
   it('2+2=4', () => {
     expect(2 + 2).toBe(4);
+  });
+});
+
+describe('CreateUserDto', () => {
+  it('should validate email, name, and password', async () => {
+    // Arrange
+    const dto = new CreateUserDto();
+    dto.email = 'ajie.io@yahoo.com';
+    dto.name = 'Ajie';
+    dto.password = 'password123';
+    // Act
+    const errors = await validate(dto);
+    // Assert
+    expect(errors.length).toBe(0);
   });
 });
