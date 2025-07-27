b6d4198f7a1c0c0a6840df5e929c6bed99a2f7d6
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-27 16:00:56 -0400

    106 Adding Protected Profile Endpoint

--- nestjs-intro/src/users/auth/auth.controller.ts
+++ nestjs-intro/src/users/auth/auth.controller.ts
@@ -2,9 +2,12 @@ import {
   Body,
   ClassSerializerInterceptor,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
+  NotFoundException,
   Post,
+  Request,
   SerializeOptions,
   UseInterceptors,
 } from '@nestjs/common';
@@ -13,6 +16,8 @@ import { User } from '../user.entity';
 import { CreateUserDto } from '../create-user.dto';
 import { LoginDto } from '../login.dto';
 import { LoginResponse } from '../login.response';
+import { AuthRequest } from '../auth.request';
+import { UserService } from '../user/user.service';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -20,7 +25,10 @@ import { LoginResponse } from '../login.response';
   strategy: 'excludeAll',
 })
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
@@ -37,5 +45,16 @@ export class AuthController {
     );
 
     return new LoginResponse({ accessToken });
+  }
+
+  @Get('/profile')
+  async profile(@Request() request: AuthRequest): Promise<User> {
+    const user = await this.userService.findOne(request.user.sub);
+
+    if (user) {
+      return user;
+    }
+
+    throw new NotFoundException();
   }
 }
