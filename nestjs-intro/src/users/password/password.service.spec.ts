cb189daf40b2f4efff3a9c907be987e3377c1738
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-26 20:55:12 -0400

    88 Testing PasswordService - Mock & Spy

--- nestjs-intro/src/users/password/password.service.spec.ts
+++ nestjs-intro/src/users/password/password.service.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasswordService } from './password.service';
+import * as bcrypt from 'bcrypt';
+
+jest.mock('bcrypt', () => ({
+  hash: jest.fn(),
+  compare: jest.fn(),
+}));
 
 describe('PasswordService', () => {
   let service: PasswordService;
@@ -14,5 +20,14 @@ describe('PasswordService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should hash and verify password', async () => {
+    const mockedHash = 'hashedPassword';
+    (bcrypt.hash as jest.Mock).mockResolvedValue(mockedHash);
+    const password = 'password123';
+    const result = await service.hashPassword(password);
+    expect(bcrypt.hash).toHaveBeenCalledWith(password, service['SALT_ROUNDS']);
+    expect(result).toBe(mockedHash);
   });
 });
