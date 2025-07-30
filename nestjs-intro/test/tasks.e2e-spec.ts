b3c4568e153eea9024fda1195e7c0e8996d056ea
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-07-29 22:46:10 -0400

    115 Preparing the Battlefield: E2E Test Setup for the Tasks API

---
+++ nestjs-intro/test/tasks.e2e-spec.ts
@@ -0,0 +1,56 @@
+import { TaskStatus } from './../src/tasks/tasks.model';
+import { PasswordService } from './../src/users/password/password.service';
+import { Role } from './../src/users/role.enum';
+import { User } from './../src/users/user.entity';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { TestSetup } from './utils/test-setup';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+
+describe('Tasks (e2e)', () => {
+  let testSetup: TestSetup;
+  let authToken: string;
+  let taskId: string;
+
+  const testUser = {
+    email: 'test@example.com',
+    password: 'Password123!',
+    name: 'Test User',
+  };
+
+  beforeEach(async () => {
+    testSetup = await TestSetup.create(AppModule);
+
+    await request(testSetup.app.getHttpServer())
+      .post('/auth/register')
+      .send(testUser)
+      .expect(201);
+
+    const loginResponse = await request(testSetup.app.getHttpServer())
+      .post('/auth/login')
+      .send(testUser)
+      .expect(201);
+
+    authToken = loginResponse.body.accessToken;
+
+    const response = await request(testSetup.app.getHttpServer())
+      .post('/tasks')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({
+        title: 'Test Task',
+        description: 'Test Desc',
+        status: TaskStatus.OPEN,
+        labels: [{ name: 'test' }],
+      });
+    taskId = response.body.id;
+  });
+
+  afterEach(async () => {
+    await testSetup.cleanup();
+  });
+
+  afterAll(async () => {
+    await testSetup.teardown();
+  });
+});
